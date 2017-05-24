61cf0ed3b4c0ce881c2d1446c63b57b14c0a0d9c
Sam Nouk <snoukeu@gmail.com>
2017-05-24 11:52:17 +0200

    starfoula

--- js/home_page.js
+++ js/home_page.js
@@ -6,6 +6,10 @@ $('#fleche').click(function()
     $('html,body').animate({
         scrollTop: $("#nos_principes").offset().top 
     }, 'slow');
+    
+ 
+    
+    
 });
 
  $("#slider").slick({
@@ -17,4 +21,28 @@ $('.autoplay').slick({
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
+  arrows : false
 });
+
+
+/*  Tentative d'animation en fonction du scroll
+    sur la page 
+    
+   $(window).scroll(function()
+{
+    var sticky = $ ('#trait'),
+        bool = false,
+        posScroll = $(document).scrollTop(),
+        possCamion = $('#nos_partenaires').position().top,
+        windowHeight = $(window).height();
+       console.log($(window).height());
+    console.log("tamere"+$('#nos_partenaires').position().top);
+       console.log('srcoll : '+ posScroll);
+        
+    
+    if( posScroll > windowHeight - possCamion +200) sticky.removeClass('bouge'),sticky.addClass('stop');
+    else sticky.removeClass('stop'), sticky.addClass('bouge');
+})*/
+   
+   
+   
